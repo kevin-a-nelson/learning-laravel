c6994f98e92f5c9995e65f590566378c851fb326
Kevin Nelson <nelsonkevin767@gmail.com>
2024-04-28 18:32:35 +0900

    Added timestamps to comments

--- backend/app/Http/Controllers/CommentController.php
+++ backend/app/Http/Controllers/CommentController.php
@@ -3,18 +3,35 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
+use App\Models\Comment;
 
 class CommentController extends Controller
 {
     public function index()
     {
+        $comments = Comment::orderByDesc('created_at')->get();
 
-        //
+        return response()->json(
+            $comments->map(function ($comment) {
+                return [
+                    "id" => $comment->id,
+                    "text" => $comment->text,
+                    "user" => User::where('id', $comment->userId)->first(),
+                    "userId" => $comment->userId
+                ];
+            })->toArray()
+        );
     }
 
-    public function create()
+    public function create(Request $request)
     {
-        //
+        $comment = new Comment();
+        $comment->text = $request->text;
+        $comment->userId = $request->userId;
+        $comment->save();
+
+        return $comment;
     }
 
     public function show(string $id)
